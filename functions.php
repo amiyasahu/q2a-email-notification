4dab5fab2c4e71329fea9117f7ae48792355814c
Amiya Sahu <amiya053@gmail.com>
2014-07-03 01:22:57 +0530

    Small fixes in the code

--- functions.php
+++ functions.php
@@ -29,17 +29,19 @@ function set_all_notification_options() {
             $minimum_user_point_option = !!qa_post_text('ami_email_notf_min_point');
             if ($minimum_user_point_option) { //if minimum point option is checked 
                   $minimum_user_point_value = qa_post_text('ami_email_notf_min_point_val');
-                  if (!!$minimum_user_point_value && is_numeric($minimum_user_point_value) && $minimum_user_point_value > 0) { //if the minimum point value is provided then only set else reset
+                  if (!!$minimum_user_point_value && is_numeric($minimum_user_point_value) && $minimum_user_point_value > 0) { 
+                        //if the minimum point value is provided then only set else reset
                         qa_opt('ami_email_notf_min_point', $minimum_user_point_option);
                         qa_opt('ami_email_notf_min_point_val', (int) $minimum_user_point_value);
-                  } else if (!is_numeric($minimum_user_point_value) || $minimum_user_point_value <= 0) {
+                  } else if (!!$minimum_user_point_value && (!is_numeric($minimum_user_point_value) || $minimum_user_point_value <= 0)) {
+                        // the minimum_user_point_value is set but the value is not valid
                         reset_all_notification_points_options();
                         //send a error message to UI 
-                        $error['enter_point_value'] = qa_lang('point_value_should_numeric');
+                        $error['enter_point_value'] = qa_lang('notify/point_value_should_numeric');
                   } else {
                         reset_all_notification_points_options();
                         //send a error message to UI 
-                        $error['enter_point_value'] = qa_lang('point_value_required'); ;
+                        $error['enter_point_value'] = qa_lang('notify/point_value_required'); ;
                   }
             } else {
                   reset_all_notification_points_options();
@@ -48,7 +50,7 @@ function set_all_notification_options() {
             //if none of the elements are selected disable the plugin and send a error message UI 
             qa_opt('ami_email_notf_enable_plugin', false);
             reset_all_notification_options();
-            $error['no_options_selected'] = qa_lang('choose_atleast_one_opt');
+            $error['no_options_selected'] = qa_lang('notify/choose_atleast_one_opt');
       }
       return $error;
 }
